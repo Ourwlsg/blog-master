ff890e679330082d63e1d91d458c31a95e85a44f
Our_wlsg <zhucc97@gmail.com>
2021-07-08 20:37:49 +0800

    更新分页接口

--- blog-server/src/main/java/pers/fjl/server/config/MyBatisPlusConfig.java
+++ blog-server/src/main/java/pers/fjl/server/config/MyBatisPlusConfig.java
@@ -1,8 +1,10 @@
 package pers.fjl.server.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,11 +21,35 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @Configuration
 public class MyBatisPlusConfig {
+
+    // 已过期
+    //    @Bean
+    //    public PaginationInterceptor paginationInterceptor() {
+    //        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+    //        paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
+    //        return paginationInterceptor;
+    //    }
+
+//    @Bean
+//    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+//        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+//        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+//
+//        // 设置请求的页面大于最大页后操作，true调回到首页，false 继续请求  默认false
+//        paginationInnerInterceptor.setOverflow(true) ;
+//        // 设置最大单页限制数量，默认50日条，-1不受限制
+//        paginationInnerInterceptor.setMaxLimit(50L);
+//        //开启count 的join优化，只针对部分left join
+//        mybatisPlusInterceptor.addInnerInterceptor(paginationInnerInterceptor);
+//        return mybatisPlusInterceptor;
+//    }
+
+    // 最新版
     @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
-        paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
-        return paginationInterceptor;
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
     }
 
     //注册乐观锁
